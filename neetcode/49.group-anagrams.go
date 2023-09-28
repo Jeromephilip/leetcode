0b9f326d2afad72c2e7f39458c9035a9ca6f5a13
Jeromephilip <jeromephilip03@gmail.com>
2023-09-28 02:01:19 -0400

    group-anagrams passed

--- neetcode/49.group-anagrams.go
+++ neetcode/49.group-anagrams.go
@@ -4,6 +4,12 @@
  * [49] Group Anagrams
  */
 
+/* Initial idea is to create a map where the key is an array
+containing the occurences of a given character in a string and 
+the value is all the strings that match the occurence key. Another
+way to solve this is having the key as a sorted string
+*/
+
 // @lc code=start
 func groupAnagrams(strs []string) [][]string {
 	m := make(map[[26]int][]string)
