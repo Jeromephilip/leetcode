7e0c285b4440dc0f6669c6728958a80f6ce03ffc
Jeromephilip <jeromephilip03@gmail.com>
2023-09-28 01:20:01 -0400

    valid anagram passed

---
+++ neetcode/242.valid-anagram.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=242 lang=golang
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+    var sArray [26]int
+	var tArray [26]int
+	for i := 0; i < len(s); i++ {
+		sArray[int(s[i]) - 97]++;
+		tArray[int(t[i]) - 97]++;
+	}
+
+	for i:= 0; i < 26; i++ {
+		if sArray[i] != tArray[i] {
+			return false
+		}
+	}
+
+	return true
+	
+}
+// @lc code=end
+
